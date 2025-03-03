4432f4419155d851fe4e1a961fb21ee12166ae48
Hanelore <80646753+iamhany@users.noreply.github.com>
2025-03-03 21:18:01 +0200

    Update Status entity

--- src/main/java/com/example/bookstore/entities/Status.java
+++ src/main/java/com/example/bookstore/entities/Status.java
@@ -1,5 +1,32 @@
 package com.example.bookstore.entities;
 
 public enum Status {
-    PENDING, IN_PROGRESS, DELAYED, FINISHED, CANCELED
-}
+    PENDING {
+        @Override
+        public boolean isNextStatePossible(Status status) {
+            return status == Status.IN_PROGRESS  || status == Status.CANCELED;
+        }
+    }, IN_PROGRESS {
+        @Override
+        public boolean isNextStatePossible(Status status) {
+            return status == Status.FINISHED;
+        }
+    }, DELAYED {
+        @Override
+        public boolean isNextStatePossible(Status status) {
+            return status == Status.FINISHED;
+        }
+    }, FINISHED {
+        @Override
+        public boolean isNextStatePossible(Status status) {
+            return false;
+        }
+    }, CANCELED {
+        @Override
+        public boolean isNextStatePossible(Status status) {
+            return false;
+        }
+    };
+
+    public abstract boolean isNextStatePossible(Status status);
+    }
